20755c79993b88a8354d3ef050744a449999caa2
unknown <fengqi.mao@energysh.com>
2020-11-18 19:54:18 +0800

    4

--- home/src/main/java/com/codearms/maoqiqi/one/ui/HomeActivity.kt
+++ home/src/main/java/com/codearms/maoqiqi/one/ui/HomeActivity.kt
@@ -2,11 +2,18 @@ package com.codearms.maoqiqi.one.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.codearms.maoqiqi.one.home.R
+import com.codearms.maoqiqi.one.listener.OnToolbarListener
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), OnToolbarListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onToolbar(toolbar: Toolbar?) {
+        setSupportActionBar(toolbar)
     }
 }
